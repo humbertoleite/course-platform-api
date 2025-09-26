d7a65b97ceb9485553e636ef8497bb122ccbf759
Humberto <humberto@onecommerce.es>
2025-09-26 10:25:15 +0200

    Add Comment model, factory, and migration for comments table

---
+++ database/migrations/2025_09_26_082436_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->tinyInteger('rating')->comment('1 to 5 stars'); // 1-5
+            $table->text('content');
+            $table->timestamps();
+            $table->unique(['user_id', 'course_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
